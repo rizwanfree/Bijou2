5d15eb972d44b9aed6ccbbcda4e88a390926ff71
rizwanfree <rizwanfreelance@gmail.com>
2024-11-29 16:07:05 +0500

    Navbar

--- mainweb/urls.py
+++ mainweb/urls.py
@@ -3,6 +3,12 @@ from django.urls import path, include
 
 from . import views
 
+app_name = 'mainweb'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('gallery', views.gallery, name='gallery'),
+    path('services', views.services, name='services'),
+    path('contact', views.contact, name='contact'),
 ]
--- mainweb/views.py
+++ mainweb/views.py
@@ -5,3 +5,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main-web/index-2.html')
+
+
+def about(request):
+    return render(request, 'main-web/about.html')
+
+
+def services(request):
+    return render(request, 'main-web/index-2.html')
+
+
+def gallery(request):
+    return render(request, 'main-web/gallery.html')
+
+
+def contact(request):
+    return render(request, 'main-web/contacts.html')
+
+
