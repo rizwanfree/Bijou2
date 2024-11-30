94f667f26f893a0d1630f009719db2f691507307
rizwanfree <121812303+rizwanfree@users.noreply.github.com>
2024-11-30 15:51:12 +0500

    gallery

--- mainweb/urls.py
+++ mainweb/urls.py
@@ -8,7 +8,7 @@ app_name = 'mainweb'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('gallery', views.gallery, name='gallery'),
-    path('services', views.services, name='services'),
-    path('contact', views.contact, name='contact'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
 ]
