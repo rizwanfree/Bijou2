e8755613767a52e6c513f10f7bd622f78c563e21
rizwanfree <121812303+rizwanfree@users.noreply.github.com>
2024-12-06 16:26:19 +0500

    profile model update

--- users/models.py
+++ users/models.py
@@ -1,13 +1,17 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-from django.contrib.auth.models import AbstractUser
 from accomodations.models import Property
 
 class TenantProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='tenant_profile')
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    middle_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    email = models.EmailField(max_length=50, blank=True, null=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     address = models.TextField(blank=True, null=True)  # Tenant's address
+
 
     def __str__(self):
         return f"Tenant: {self.user.username}"
@@ -17,6 +21,12 @@ class TenantProfile(models.Model):
 
 class PropertyManagerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='manager_profile')
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    middle_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    email = models.EmailField(max_length=50, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
     managed_properties = models.ManyToManyField(Property, related_name='assigned_managers', blank=True)
 
     def __str__(self):
